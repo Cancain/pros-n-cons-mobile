fe9dce66b34e53345e8bf5be679eb4f99c464ca7
Cancain <eriksson.tomas@gmail.com>
2019-06-06 16:42:40 +0200

    added a header container

--- containers/Layout/Layout.js
+++ containers/Layout/Layout.js
@@ -1,19 +1,12 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import List from "../List/List";
+import Header from "../Header/Header";
 
 const Layout = props => {
-  const styles = StyleSheet.create({
-    Headline: {
-      fontWeight: "bold",
-      textAlign: "center",
-      fontSize: 30,
-      marginTop: 20
-    }
-  });
   return (
     <View>
-      <Text style={styles.Headline}>Pros 'N Cons</Text>
+      <Header />
       <List />
     </View>
   );
